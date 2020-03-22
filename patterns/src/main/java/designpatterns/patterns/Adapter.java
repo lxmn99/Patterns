0ea25a39c00e03351cb3e50066baec03e760f577
lxmn99 <60390751+lxmn99@users.noreply.github.com>
2020-03-22 20:56:44 +0530

    Add files via upload

---
+++ patterns/src/main/java/designpatterns/patterns/Adapter.java
@@ -0,0 +1,75 @@
+package designpatterns.patterns;
+
+interface Bird 
+{ 
+ 
+    public void fly(); 
+    public void makeSound(); 
+} 
+  
+class Sparrow implements Bird 
+{ 
+    
+    public void fly() 
+    { 
+        System.out.println("Flying"); 
+    } 
+    public void makeSound() 
+    { 
+        System.out.println("Chirp Chirp"); 
+    } 
+}
+
+
+interface ToyDuck 
+{ 
+  // target interface 
+  // toyducks dont fly they just make 
+  // squeaking sound 
+  public void squeak(); 
+} 
+
+class PlasticToyDuck implements ToyDuck 
+{ 
+  public void squeak() 
+  { 
+      System.out.println("Squeak"); 
+  } 
+} 
+
+class BirdAdapter implements ToyDuck 
+{ 
+  Bird bird;
+  public BirdAdapter(Bird bird) 
+  { 
+      
+      this.bird = bird; 
+  } 
+
+  public void squeak() 
+  { 
+      bird.makeSound(); 
+  } 
+} 
+
+public class Adapter 
+{ 
+  public static void main(String args[]) 
+  { 
+      Sparrow sparrow = new Sparrow(); 
+      ToyDuck toyDuck = new PlasticToyDuck(); 
+
+    
+      ToyDuck birdAdapter = new BirdAdapter(sparrow); 
+
+      System.out.println("Sparrow..."); 
+      sparrow.fly(); 
+      sparrow.makeSound(); 
+
+      System.out.println("ToyDuck..."); 
+      toyDuck.squeak(); 
+  
+      System.out.println("BirdAdapter..."); 
+      birdAdapter.squeak(); 
+  } 
+} 
---
+++ patterns/src/main/java/designpatterns/patterns/Singleton.java
@@ -0,0 +1,18 @@
+package designpatterns.patterns;
+
+public class Singleton 
+{ 
+	private static Singleton s; 
+
+	private Singleton() 
+	{
+		
+	} 
+
+	public static Singleton getInstance() 
+	{ 
+		if (s==null) 
+			s = new Singleton(); 
+		return s; 
+	} 
+} 
